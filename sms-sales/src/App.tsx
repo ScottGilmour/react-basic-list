6405c98371415078ee55e77bf8bc9c3618239dd1
Scott Gilmour <scottryangilmour@gmail.com>
2019-01-19 11:28:45 -0500

    Fin, easy

--- sms-sales/src/App.tsx
+++ sms-sales/src/App.tsx
@@ -1,19 +1,63 @@
 import * as React from 'react';
 import './App.css';
 
-import logo from './logo.svg';
+import { Contact } from './class/contact.class';
 
-class App extends React.Component {
+import logo from './logo.svg';
+import { ContactList } from './components/contacts-list/contacts-list';
+import { SMSProps, SMSState, AddContactState } from './interfaces';
+import { AddContact } from './components/add-contact/add-contact';
+
+class App extends React.Component<SMSProps, SMSState> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      contacts: []
+    }
+
+    this.handleNewContact = this.handleNewContact.bind(this);
+  }
+  
+  public componentDidMount() {
+    
+    const contact: Contact = new Contact();
+    contact.name = 'Frank Sherbet';
+    contact.company = 'Dunder Mifflin';
+    contact.phoneNumber = '9052205327';
+
+    
+    this.setState({contacts: [contact]});
+  }
+
+  private handleNewContact(contactInfo: AddContactState) {
+      let contact: Contact = new Contact();
+      contact.name = contactInfo.name;
+      contact.company = contactInfo.company;
+      contact.phoneNumber = contactInfo.phoneNumber;
+
+      
+      this.setState({
+        contacts: [...this.state.contacts, contact]
+      });
+  }
+
   public render() {
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        
+        <AddContact 
+            onNewContact={this.handleNewContact}
+
+          
+          />
+          
+          <ContactList contacts={this.state.contacts} />
       </div>
     );
   }
